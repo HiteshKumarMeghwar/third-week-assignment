30bf5d41cf2e818bb5adac958f03abf22647e5a5
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2022-12-24 12:51:57 +0500

    updated

--- assignment/controllers/static.go
+++ assignment/controllers/static.go
@@ -2,15 +2,38 @@ package controllers
 
 import (
 	"assignment/views"
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/sessions"
 )
 
-// var store = sessions.NewCookieStore([]byte("mysession"))
+// var db = database.Connect()
+var store = sessions.NewCookieStore([]byte("super-secret"))
 
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Println(tpl.HTMLtpl.Name())
 		// fmt.Println(r.URL.Path)
+		// tpl.Execute(w, nil)
+		if r.URL.Path == "/" {
+			session, _ := store.Get(r, "session")
+			_, ok := session.Values["userId"]
+			fmt.Println("ok: ", ok)
+			if !ok {
+				http.Redirect(w, r, "/login", http.StatusFound) // http.StatusFound is 302
+				return
+			}
+			tpl.Execute(w, nil)
+		} else if r.URL.Path == "/login" {
+			session, _ := store.Get(r, "session")
+			_, ok := session.Values["userId"]
+			fmt.Println("ok: ", ok)
+			if ok {
+				http.Redirect(w, r, "/dashboard", http.StatusFound) // http.StatusFound is 302
+				return
+			}
+		}
 		tpl.Execute(w, nil)
 	}
 }
--- assignment/main.go
+++ assignment/main.go
@@ -5,10 +5,13 @@ import (
 	"assignment/controllers"
 	"assignment/views"
 	"fmt"
+	"html/template"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/sessions"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
@@ -16,12 +19,16 @@ func main() {
 	db := database.Connect()
 	defer db.Close()
 
+	/* Initialization of Session */
+	var store = sessions.NewCookieStore([]byte("super-secret"))
+
 	/* _, err := db.Exec(`
 
 	CREATE TABLE IF NOT EXISTS users(
 		id SERIAL PRIMARY KEY,
 		name TEXT,
-		email TEXT UNIQUE NOT NULL
+		username TEXT UNIQUE NOT NULL,
+		password TEXT
 	)`)
 
 	if err != nil {
@@ -33,6 +40,60 @@ func main() {
 	route.Get("/", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "dashboard.gohtml")))))
 
 	route.Get("/login", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "login.gohtml")))))
+
+	route.Post("/loginAuth", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		r.ParseForm()
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		fmt.Println("Username: ", username, "Password: ", password)
+
+		tpl, err := template.ParseFiles(filepath.Join("templates", "login.gohtml"))
+		if err != nil {
+			panic(err)
+		}
+
+		var userId int
+		var pass string
+		// stmt := "SELECT id, password FROM users WHERE username = ?"
+		// row := db.QueryRow(stmt, username)
+		// err = row.Scan(&userId, &pass)
+		err = db.QueryRow("SELECT id, password FROM users WHERE username = ?", username).Scan(&userId, &pass)
+		fmt.Println("hash from db: ", pass)
+		if err != nil {
+			fmt.Println("error selecting Hash in db by Username")
+			tpl.Execute(w, nil)
+			return
+		}
+
+		err = bcrypt.CompareHashAndPassword([]byte(pass), []byte(password))
+		if err == nil {
+			tpl, err := template.ParseFiles(filepath.Join("templates", "dashboard.gohtml"))
+			if err != nil {
+				panic(err)
+			}
+			session, _ := store.Get(r, "session")
+			session.Values["userId"] = userId
+			session.Save(r, w)
+			tpl.Execute(w, nil)
+			return
+		}
+
+		fmt.Println("incorrect password")
+		tpl.Execute(w, nil)
+	})
+
+	route.Get("/logout", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("logouting .........! ")
+		session, _ := store.Get(r, "session")
+		delete(session.Values, "userId")
+		session.Save(r, w)
+		tpl, err := template.ParseFiles(filepath.Join("templates", "login.gohtml"))
+		if err != nil {
+			panic(err)
+		}
+		tpl.Execute(w, nil)
+	})
 
 	route.Get("/register", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "register.gohtml")))))
 
